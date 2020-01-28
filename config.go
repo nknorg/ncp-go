7bf9ec04d8ada40ed813c55e7ae66b1252865ca6
Yilun <zyl.skysniper@gmail.com>
2020-01-28 15:42:50 -0800

    Add default config and remove dialtime from config
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- config.go
+++ config.go
@@ -1,5 +1,7 @@
 package ncp
 
+import "github.com/imdario/mergo"
+
 type Config struct {
 	NonStream                    bool
 	SessionWindowSize            int32 // in bytes
@@ -14,5 +16,31 @@ type Config struct {
 	MaxRetransmissionTimeout     int32 // in millisecond
 	SendAckInterval              int32 // in millisecond
 	CheckTimeoutInterval         int32 // in millisecond
-	DialTimeout                  int32 // in millisecond
+}
+
+var DefaultConfig = Config{
+	NonStream:                    false,
+	SessionWindowSize:            4 << 20,
+	MTU:                          1024,
+	InitialConnectionWindowSize:  16,
+	MaxConnectionWindowSize:      256,
+	MinConnectionWindowSize:      1,
+	MaxAckSeqListSize:            32,
+	FlushInterval:                10,
+	Linger:                       1000,
+	InitialRetransmissionTimeout: 5000,
+	MaxRetransmissionTimeout:     10000,
+	SendAckInterval:              50,
+	CheckTimeoutInterval:         50,
+}
+
+func MergeConfig(conf *Config) (*Config, error) {
+	merged := DefaultConfig
+	if conf != nil {
+		err := mergo.Merge(&merged, conf, mergo.WithOverride)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &merged, nil
 }
